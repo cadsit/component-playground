19139068e5e938ca82fcb156485e7384022ed17e
Paula Lavalle <lavalle.paula@gmail.com>
2016-05-30 13:41:00 -0700

    Remove CodeMirror

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -1,8 +1,5 @@
 /* eslint new-cap:0 no-unused-vars:0 */
 import React from "react";
-import CodeMirror from "codemirror";
-
-require("codemirror/mode/jsx/jsx");
 
 const Editor = React.createClass({
   propTypes: {
@@ -15,44 +12,11 @@ const Editor = React.createClass({
     style: React.PropTypes.object,
     className: React.PropTypes.string
   },
-  componentDidMount() {
-    this.editor = CodeMirror.fromTextArea(this.refs.editor, {
-      mode: "jsx",
-      lineNumbers: false,
-      lineWrapping: true,
-      smartIndent: false,
-      matchBrackets: true,
-      theme: this.props.theme,
-      readOnly: this.props.readOnly
-    });
-
-    if (Array.isArray(this.props.selectedLines)) {
-      this.props.selectedLines.forEach((lineNumber) => {
-        this.editor.addLineClass(lineNumber, "wrap", "CodeMirror-activeline-background");
-      });
-    }
-
-    this.editor.on("change", this._handleChange);
-  },
-
-  componentDidUpdate() {
-    if (this.props.readOnly || this.props.external) {
-      this.editor.setValue(this.props.codeText);
-    }
-  },
-
-  _handleChange() {
-    if (!this.props.readOnly && this.props.onChange) {
-      this.props.onChange(this.editor.getValue());
-    }
-  },
 
   render() {
-    const editor = <textarea ref="editor" defaultValue={this.props.codeText} />;
-
     return (
-      <div style={this.props.style} className={this.props.className}>
-        {editor}
+      <div>
+        COMPONENT PLAYGROUND!!
       </div>
     );
   }
